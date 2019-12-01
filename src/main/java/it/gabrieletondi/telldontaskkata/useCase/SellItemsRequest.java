21c80a34eb970bd16bcef4a0d5591365f875361a
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-12-02 01:49:06 +0500

    format

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/SellItemsRequest.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/SellItemsRequest.java
@@ -3,13 +3,14 @@ package it.gabrieletondi.telldontaskkata.useCase;
 import java.util.List;
 
 class SellItemsRequest {
-    private List<SellItemRequest> requests;
+
+  private List<SellItemRequest> requests;
 
   SellItemsRequest(List<SellItemRequest> requests) {
-        this.requests = requests;
-    }
+    this.requests = requests;
+  }
 
   List<SellItemRequest> getRequests() {
-        return requests;
-    }
+    return requests;
+  }
 }
