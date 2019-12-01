6774b7d387513b766ab6b8cee9b4ffcccea3b748
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-12-02 02:40:25 +0500

    rename field

--- src/test/java/it/gabrieletondi/telldontaskkata/useCase/OrderApprovalUseCaseTest.java
+++ src/test/java/it/gabrieletondi/telldontaskkata/useCase/OrderApprovalUseCaseTest.java
@@ -16,47 +16,47 @@ import org.junit.Test;
 public class OrderApprovalUseCaseTest {
 
   private final TestOrderRepository orderRepository = new TestOrderRepository();
-  private final OrderApprovalUseCase useCase = new OrderApprovalUseCase(orderRepository);
+  private final OrderApprovalUseCase approval = new OrderApprovalUseCase(orderRepository);
 
   @Test
   public void approvedExistingOrder() throws Exception {
     orderRepository.add(anOrder().with(OrderStatus.CREATED).build());
-    useCase.run(new ApproveOrderRequest(1));
+    approval.run(new ApproveOrderRequest(1));
     assertTrue(orderRepository.savedOrderHas(OrderStatus.APPROVED));
   }
 
   @Test
   public void rejectedExistingOrder() throws Exception {
     orderRepository.add(anOrder().with(OrderStatus.CREATED).build());
-    useCase.run(new RejectOrderRequest(1));
+    approval.run(new RejectOrderRequest(1));
     assertTrue(orderRepository.savedOrderHas(OrderStatus.REJECTED));
   }
 
   @Test(expected = RejectedOrderCannotBeApprovedException.class)
   public void cannotApproveRejectedOrder() throws Exception {
     orderRepository.add(anOrder().with(OrderStatus.REJECTED).build());
-    useCase.run(new ApproveOrderRequest(1));
+    approval.run(new ApproveOrderRequest(1));
     assertTrue(orderRepository.orderIsNotSaved());
   }
 
   @Test(expected = ApprovedOrderCannotBeRejectedException.class)
   public void cannotRejectApprovedOrder() throws Exception {
     orderRepository.add(anOrder().with(OrderStatus.APPROVED).build());
-    useCase.run(new RejectOrderRequest(1));
+    approval.run(new RejectOrderRequest(1));
     assertTrue(orderRepository.orderIsNotSaved());
   }
 
   @Test(expected = ShippedOrdersCannotBeChangedException.class)
   public void shippedOrdersCannotBeApproved() throws Exception {
     orderRepository.add(anOrder().with(OrderStatus.SHIPPED).build());
-    useCase.run(new ApproveOrderRequest(1));
+    approval.run(new ApproveOrderRequest(1));
     assertTrue(orderRepository.orderIsNotSaved());
   }
 
   @Test(expected = ShippedOrdersCannotBeChangedException.class)
   public void shippedOrdersCannotBeRejected() throws Exception {
     orderRepository.add(anOrder().with(OrderStatus.SHIPPED).build());
-    useCase.run(new RejectOrderRequest(1));
+    approval.run(new RejectOrderRequest(1));
     assertTrue(orderRepository.orderIsNotSaved());
   }
 }
