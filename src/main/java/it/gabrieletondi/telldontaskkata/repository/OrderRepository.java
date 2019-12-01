bc6048a56bdd894769509476e8727c666130aa10
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-12-02 02:47:21 +0500

    format

--- src/main/java/it/gabrieletondi/telldontaskkata/repository/OrderRepository.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/repository/OrderRepository.java
@@ -3,7 +3,8 @@ package it.gabrieletondi.telldontaskkata.repository;
 import it.gabrieletondi.telldontaskkata.domain.Order;
 
 public interface OrderRepository {
-    void save(Order order);
 
-    Order orderWith(int orderId);
+  void save(Order order);
+
+  Order orderWith(int orderId);
 }
