a45d8045d1b1ad5c10928dfd1a471a246537890a
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-12-02 16:25:19 +0500

    moving external dependency up

--- src/main/java/it/gabrieletondi/telldontaskkata/repository/ProductCatalog.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/repository/ProductCatalog.java
@@ -1,12 +1,9 @@
 package it.gabrieletondi.telldontaskkata.repository;
 
-import it.gabrieletondi.telldontaskkata.domain.Product;
 import it.gabrieletondi.telldontaskkata.domain.Products;
 import java.util.List;
 
 public interface ProductCatalog {
-
-  Product productWith(String name);
 
   boolean doesNotContainsAllProductsWith(List<String> productNames);
 
