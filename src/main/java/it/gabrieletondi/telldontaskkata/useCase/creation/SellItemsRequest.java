d489ef9a5dc1f5a021f749012ef553dc44f54a7c
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-12-02 19:48:30 +0500

    reversed the other one as well

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/creation/SellItemsRequest.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/creation/SellItemsRequest.java
@@ -3,8 +3,8 @@ package it.gabrieletondi.telldontaskkata.useCase.creation;
 import it.gabrieletondi.telldontaskkata.domain.Order;
 import it.gabrieletondi.telldontaskkata.domain.Product;
 import it.gabrieletondi.telldontaskkata.domain.Products;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SellItemsRequest {
 
@@ -24,8 +24,6 @@ public class SellItemsRequest {
   }
 
   List<String> productNames() {
-    List<String> productNames = new ArrayList<>();
-    requests.forEach(r -> r.addProductNameTo(productNames));
-    return productNames;
+    return requests.stream().map(SellItemRequest::getProductName).collect(Collectors.toList());
   }
 }
