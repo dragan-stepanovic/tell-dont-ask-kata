ff11f20e013dae85ca53ab17b975f75abe5a1a75
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-12-02 12:28:08 +0500

    removing not used methods

--- src/test/java/it/gabrieletondi/telldontaskkata/doubles/TestOrderRepository.java
+++ src/test/java/it/gabrieletondi/telldontaskkata/doubles/TestOrderRepository.java
@@ -1,7 +1,6 @@
 package it.gabrieletondi.telldontaskkata.doubles;
 
 import it.gabrieletondi.telldontaskkata.domain.Order;
-import it.gabrieletondi.telldontaskkata.domain.OrderStatus;
 import it.gabrieletondi.telldontaskkata.domain.OrderStatusNew;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
 import java.util.ArrayList;
@@ -31,10 +30,6 @@ public class TestOrderRepository implements OrderRepository {
 
   public boolean savedOrderIs(Order value) {
     return insertedOrder.equals(value);
-  }
-
-  public boolean savedOrderHas(OrderStatus status) {
-    return insertedOrder.has(status);
   }
 
   public boolean savedOrderHasStatus(OrderStatusNew status) {
