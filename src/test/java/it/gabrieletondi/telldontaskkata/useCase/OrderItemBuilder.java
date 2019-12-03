4d9c625582ed183fcb06b8bad2d86896c382a0aa
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-12-03 14:48:01 +0500

    separate builder

---
+++ src/test/java/it/gabrieletondi/telldontaskkata/useCase/OrderItemBuilder.java
@@ -0,0 +1,43 @@
+package it.gabrieletondi.telldontaskkata.useCase;
+
+import it.gabrieletondi.telldontaskkata.domain.OrderItem;
+import it.gabrieletondi.telldontaskkata.domain.Price;
+import it.gabrieletondi.telldontaskkata.domain.Product;
+import java.math.BigDecimal;
+
+class OrderItemBuilder {
+
+  private String productName = "";
+  private String price;
+  private int quantity;
+  private String taxedAmount;
+  private String taxAmount;
+  private BigDecimal taxPercentage;
+
+  OrderItemBuilder(int quantity, String taxedAmount, String taxAmount, BigDecimal taxPercentage) {
+    this.quantity = quantity;
+    this.taxedAmount = taxedAmount;
+    this.taxAmount = taxAmount;
+    this.taxPercentage = taxPercentage;
+  }
+
+  OrderItemBuilder forProductWithName(String productName) {
+    this.productName = productName;
+    return this;
+  }
+
+  OrderItemBuilder withPrice(String price) {
+    this.price = price;
+    return this;
+  }
+
+  OrderItemBuilder forQuantity(int quantity) {
+    this.quantity = quantity;
+    return this;
+  }
+
+  OrderItem build() {
+    return new OrderItem(new Product(productName, new Price(new BigDecimal(price), taxPercentage)), quantity,
+        new BigDecimal(taxedAmount), new BigDecimal(taxAmount));
+  }
+}
